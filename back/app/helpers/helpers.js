20025d241b6e07d9c7c387f51218795fa1c80222
Lautaro Silva <lautaro.silva@despegar.com>
2022-12-28 21:15:37 -0300

    se cambio el endpoint de barrios por zona por uno que retorne barrios, puntos y habitantes por zona

--- back/app/helpers/helpers.js
+++ back/app/helpers/helpers.js
@@ -1,34 +1,119 @@
-
 function quitarAcentos(cadena) {
-    const acentos = {'á':'a','é':'e','í':'i','ó':'o','ú':'u','Á':'A','É':'E','Í':'I','Ó':'O','Ú':'U'};
-    return cadena.split('').map( letra => acentos[letra] || letra).join('').toString();
+    const acentos = {
+        'á': 'a',
+        'é': 'e',
+        'í': 'i',
+        'ó': 'o',
+        'ú': 'u',
+        'Á': 'A',
+        'É': 'E',
+        'Í': 'I',
+        'Ó': 'O',
+        'Ú': 'U'
+    };
+    return cadena.split('').map(letra => acentos[letra] || letra).join('').toString();
 }
-function incrementarProvDepto(object, prov, depto) {
-    object[prov][depto].totalDepartamento = object[prov][depto].totalDepartamento + 1;
-    object[prov].totalProvincia = object[prov].totalProvincia + 1;
+
+function getTotalHabitantes(barrio) {
+    let masc = barrio.personas_genero_masc ? barrio.personas_genero_masc : 0
+    let fem = barrio.personas_genero_fem ? barrio.personas_genero_fem : 0
+    let otro = barrio.personas_genero_otrx ? barrio.personas_genero_otrx : 0
+    return masc + fem + otro
 }
-function getPointsByZone(rawdata) {
-    let pointsByZone = {};
-    rawdata.data.forEach(punto => {
-        if(punto.departamento == null) punto.departamento = punto.localidad
-        let prov = quitarAcentos(punto.provincia)
-        let depto = quitarAcentos(punto.departamento)
-        let loc = quitarAcentos(punto.localidad)
-        if (!pointsByZone[prov]) {
-            pointsByZone[prov] = { totalProvincia: 1, [depto]: { [loc]: 1, totalDepartamento: 1 } };
+
+function initProv(dataByZone, prov, depto, loc, barrio, total, punto) {
+    dataByZone[prov] = {
+        [depto]: {
+            [loc]: {
+                puntosLoc: punto ? 1 : 0,
+                barriosLoc: barrio ? 1 : 0,
+                habitantesLoc: total
+            },
+            puntosDep: punto ? 1 : 0,
+            barriosDep: barrio ? 1 : 0,
+            habitantesDep: total
+        },
+        puntosProv: punto ? 1 : 0,
+        barriosProv: barrio ? 1 : 0,
+        habitantesProv: total
+    };
+}
+
+function initDepto(dataByZone, prov, depto, loc, barrio, total, punto) {
+    dataByZone[prov][depto] = {
+        [loc]: {
+            puntosLoc: punto ? 1 : 0,
+            barriosLoc: barrio ? 1 : 0,
+            habitantesLoc: total
+        },
+        puntosDep: punto ? 1 : 0,
+        barriosDep: barrio ? 1 : 0,
+        habitantesDep: total
+    };
+    if (barrio) {
+        dataByZone[prov].barriosProv = dataByZone[prov].barriosProv + 1
+        dataByZone[prov].habitantesProv = dataByZone[prov].habitantesProv + total
+    }
+    if (punto) {
+        dataByZone[prov].puntosProv = dataByZone[prov].puntosProv + 1
+    }
+}
+
+function initLoc(dataByZone, prov, depto, loc, barrio, total, punto) {
+    dataByZone[prov][depto][loc] = {
+        puntosLoc: punto ? 1 : 0,
+        barriosLoc: barrio ? 1 : 0,
+        habitantesLoc: total
+    }
+}
+
+function increaseData(dataByZone, prov, depto, loc, barrio, total, punto) {
+    if (barrio) {
+        dataByZone[prov][depto][loc].barriosLoc = dataByZone[prov][depto][loc].barriosLoc + 1
+        dataByZone[prov][depto][loc].habitantesLoc = dataByZone[prov][depto][loc].habitantesLoc + total
+        dataByZone[prov][depto].barriosDep = dataByZone[prov][depto].barriosDep + 1
+        dataByZone[prov][depto].habitantesDep = dataByZone[prov][depto].habitantesDep + total
+        dataByZone[prov].barriosProv = dataByZone[prov].barriosProv + 1
+        dataByZone[prov].habitantesProv = dataByZone[prov].habitantesProv + total
+    }
+    if (punto) {
+        dataByZone[prov][depto][loc].puntosLoc = dataByZone[prov][depto][loc].puntosLoc + 1
+        dataByZone[prov][depto].puntosDep = dataByZone[prov][depto].puntosDep + 1
+        dataByZone[prov].puntosProv = dataByZone[prov].puntosProv + 1
+    }
+}
+
+function processData(data, dataByZone, objectType) {
+    let isBarrio = objectType === "barrio";
+    let isPunto = objectType === "punto";
+    if (data.departamento == null) data.departamento = data.localidad
+    let prov = quitarAcentos(data.provincia)
+    let depto = quitarAcentos(data.departamento)
+    let loc = quitarAcentos(data.localidad)
+    let total = getTotalHabitantes(data)
+
+    if (!dataByZone[prov]) {
+        initProv(dataByZone, prov, depto, loc, isBarrio, total, isPunto);
+    } else {
+        if (!dataByZone[prov][depto]) {
+            initDepto(dataByZone, prov, depto, loc, isBarrio, total, isPunto);
         } else {
-            if (!pointsByZone[prov][depto]) {
-                pointsByZone[prov][depto] = { [loc]: 1, totalDepartamento: 1 }
-                pointsByZone[prov].totalProvincia = pointsByZone[prov].totalProvincia + 1;
+            if (!dataByZone[prov][depto][loc]) {
+                initLoc(dataByZone, prov, depto, loc, isBarrio, total, isPunto);
             } else {
-                pointsByZone[prov][depto][loc] = !pointsByZone[prov][depto][loc] ? 1 : pointsByZone[prov][depto][loc] + 1;
-                incrementarProvDepto(pointsByZone, prov, depto)
+                increaseData(dataByZone, prov, depto, loc, isBarrio, total, isPunto);
             }
         }
-    })
-    return pointsByZone;
+    }
+}
+
+function getDataByZone(puntos, barrios) {
+    let dataByZone = {};
+    barrios.data.forEach(barrio => processData(barrio, dataByZone, "barrio"))
+    puntos.data.forEach(punto => processData(punto, dataByZone, "punto"))
+    return dataByZone;
 }
 
 module.exports = {
-    getPointsByZone,
+    getDataByZone,
 }
--- back/app/server.js
+++ back/app/server.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 const app = express();
 const fs = require("fs");
 app.use(cors())
-const {getPointsByZone} = require("./helpers/helpers");
+const {getDataByZone} = require("./helpers/helpers");
 
 // Defining get request at '/' route
 app.get('/barrios', cors(), function (req, res) {
@@ -28,12 +28,15 @@ app.get('/barrios-puntos', cors(), function (req, res) {
     res.json(data);
 });
 
-app.get('/puntos-por-zona', cors(), function (req, res) {
-    let rawdata = fs.readFileSync('./data/Puntos.json');
-    let parsedData = JSON.parse(rawdata);
-    let data = getPointsByZone(parsedData)
+app.get('/data-by-zone', cors(), function (req, res) {
+    let puntos = fs.readFileSync('./data/Puntos.json');
+    let barrios = fs.readFileSync('./data/Barrios.json');
+    let parsedPuntos = JSON.parse(puntos);
+    let parsedBarrios = JSON.parse(barrios);
+    let data = getDataByZone(parsedPuntos, parsedBarrios)
     res.json(data);
 });
+
 
 // Setting the server to listen at port 3000
 app.listen(3000, function (req, res) {
